80acc8ec4bc4c19ae47f266ad058e3d2d9aea80e
Anthony Nicholson <anthony.m.nicholson@gmail.com>
2021-06-17 15:42:06 +0900

    lesson 3: attribute binding

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+// //created a new vue app
+// const app = Vue.createApp({
+//   // data: function() {
+//   data() { //ES6 shorthand - This is the data Option
+//     return { // return object
+//       product: 'Socks',
+//       description: 'Feels good man'
+//     }
+//   }
+// })
+
+// const app = Vue.createApp({})
+// the ({}) is an Options object
+// always pass at least an empty object
+
+// Vue is reactive and have a reactivity system that handels updating for us
+
 const app = Vue.createApp({
-  // data: function() {
-  data() { //ES& shorthand
-    return {
-      product: 'Socks'
-    }
+  data() {
+      return {
+          product: 'Socks'
+      }
   }
 })
