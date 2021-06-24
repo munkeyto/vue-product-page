c04b5c48cc5d9d2ce70d5b29ae26c0865ce3ecd4
Anthony Nicholson <anthony.m.nicholson@gmail.com>
2021-06-24 13:09:45 +0900

    added button and cart components to html, added cart data and added images to variants data

--- main.js
+++ main.js
@@ -18,18 +18,19 @@
 const app = Vue.createApp({
   data() {
       return {
-          product: 'Socks',
-          description: 'green socks',
-          image: './assets/images/socks_green.jpg',
-          url: 'https://www.vuemastery.com/',
-          inventory: 7,
-          onSale: true,
-          details: ['50% cotton', '30% wool', '20% polyester'],
-          variants: [
-            { id: 2234, color: 'green' },
-            { id: 2235, color: 'blue' }
-          ],
-          sizes: ["s", "m", "l", "xl"]
-        }
+        cart: 0,
+        product: 'Socks',
+        description: 'green socks',
+        image: './assets/images/socks_green.jpg',
+        url: 'https://www.vuemastery.com/',
+        inventory: 7,
+        onSale: true,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+        ],
+        sizes: ["s", "m", "l", "xl"]
+      }
   }
 })
